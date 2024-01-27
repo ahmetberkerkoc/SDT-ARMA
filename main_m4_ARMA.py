f813003b67a6ab2c034b6982bf74a38665c7a320
ahmetberkerkoc <64761044+ahmetberkerkoc@users.noreply.github.com>
2024-01-27 15:56:51 +0300

    Update main_m4_ARMA.py

--- main_m4_ARMA.py
+++ main_m4_ARMA.py
@@ -1,5 +1,3 @@
-"""Training and evaluating a soft decision tree on the MNIST dataset."""
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -52,178 +50,178 @@ if __name__ == "__main__":
     np.random.seed(0)
     torch.manual_seed(0)
     random.seed(0)
-    m4_csv_list = random.sample(range(type_dic[m4_type]), 200)
+    M4_index = 35
     # Load data
 
     ##########################
 
     mu, sigma = 0, 0.1
-    for M4_index in m4_csv_list:
-        df = pd.read_csv(f"Dataset/Extracted_M4/{M4_index}_M4_{m4_type}.csv")
-        df = df.drop("Unnamed: 0", axis=1)
-        y = df.loc[:, "y"]
-        X = df.iloc[:, 1:]
-        e_t = np.random.normal(mu, sigma, len(X))
+    
+    df = pd.read_csv(f"Dataset/Extracted_M4/{M4_index}_M4_{m4_type}.csv")
+    df = df.drop("Unnamed: 0", axis=1)
+    y = df.loc[:, "y"]
+    X = df.iloc[:, 1:]
+    e_t = np.random.normal(mu, sigma, len(X))
 
-        X["e"] = e_t
-        X["e-1"] = 0
-        X["e-2"] = 0
-        X["e-3"] = 0
+    X["e"] = e_t
+    X["e-1"] = 0
+    X["e-2"] = 0
+    X["e-3"] = 0
 
-        X_train, X_test = X.iloc[:-forecast_horizon].to_numpy().astype(np.float32), X.iloc[
-            -forecast_horizon:
-        ].to_numpy().astype(np.float32)
-        y_train, y_test = y.iloc[:-forecast_horizon].to_numpy().astype(np.float32), y.iloc[
-            -forecast_horizon:
-        ].to_numpy().astype(np.float32)
+    X_train, X_test = X.iloc[:-forecast_horizon].to_numpy().astype(np.float32), X.iloc[
+        -forecast_horizon:
+    ].to_numpy().astype(np.float32)
+    y_train, y_test = y.iloc[:-forecast_horizon].to_numpy().astype(np.float32), y.iloc[
+        -forecast_horizon:
+    ].to_numpy().astype(np.float32)
 
-        scaler = MinMaxScaler()
-        X_train_arr = scaler.fit_transform(X_train)
-        X_test_arr = scaler.transform(X_test)
+    scaler = MinMaxScaler()
+    X_train_arr = scaler.fit_transform(X_train)
+    X_test_arr = scaler.transform(X_test)
 
-        y_train_arr = scaler.fit_transform(y_train.reshape(-1, 1))
-        y_test_arr = scaler.transform(y_test.reshape(-1, 1))
+    y_train_arr = scaler.fit_transform(y_train.reshape(-1, 1))
+    y_test_arr = scaler.transform(y_test.reshape(-1, 1))
 
-        train_features = torch.Tensor(X_train_arr).to(device)
-        train_targets = torch.Tensor(y_train_arr).to(device)
-        test_features = torch.Tensor(X_test_arr).to(device)
-        test_targets = torch.Tensor(y_test_arr).to(device)
+    train_features = torch.Tensor(X_train_arr).to(device)
+    train_targets = torch.Tensor(y_train_arr).to(device)
+    test_features = torch.Tensor(X_test_arr).to(device)
+    test_targets = torch.Tensor(y_test_arr).to(device)
 
-        train = TensorDataset(train_features, train_targets)
-        test = TensorDataset(test_features, test_targets)
+    train = TensorDataset(train_features, train_targets)
+    test = TensorDataset(test_features, test_targets)
 
-        train_loader = DataLoader(train, batch_size=batch_size, shuffle=False)
-        test_loader = DataLoader(test, batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test, batch_size=batch_size, shuffle=False)
 
-        input_dim = train_features.shape[1]
-        print(f"input_dim: {input_dim}")
-        ##########################
+    input_dim = train_features.shape[1]
+    print(f"input_dim: {input_dim}")
+    ##########################
 
-        # Model and Optimizer
-        tree = SDT(input_dim, output_dim, depth, lamda, use_cuda)
-        tree = tree.to(device)
+    # Model and Optimizer
+    tree = SDT(input_dim, output_dim, depth, lamda, use_cuda)
+    tree = tree.to(device)
 
-        optimizer = torch.optim.SGD(tree.parameters(), lr=lr)
+    optimizer = torch.optim.SGD(tree.parameters(), lr=lr)
 
-        criterion = nn.L1Loss()
+    criterion = nn.L1Loss()
 
-        train_losses = []
-        test_losses = []
+    train_losses = []
+    test_losses = []
 
-        for epoch in range(epochs):
-            print("###############")
-            print(f"epoch: {epoch}")
-            # Training
-            tree.train()
-            train_target_list = []
-            train_output_list = []
-            e_1 = 0
-            e_2 = 0
-            e_3 = 0
-            for batch_idx, (data, target) in enumerate(train_loader):
-                data[0][-3] = e_1
-                data[0][-2] = e_2
-                data[0][-1] = e_3
+    for epoch in range(epochs):
+        print("###############")
+        print(f"epoch: {epoch}")
+        # Training
+        tree.train()
+        train_target_list = []
+        train_output_list = []
+        e_1 = 0
+        e_2 = 0
+        e_3 = 0
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data[0][-3] = e_1
+            data[0][-2] = e_2
+            data[0][-1] = e_3
 
-                batch_size = data.size()[0]
-                data, target = data.to(device), target.to(device)
+            batch_size = data.size()[0]
+            data, target = data.to(device), target.to(device)
 
-                output, penalty = tree.forward(data, is_training_data=True)
+            output, penalty = tree.forward(data, is_training_data=True)
 
-                e_3 = e_2
-                e_2 = e_1
-                e_1 = target.item() - output.item()
+            e_3 = e_2
+            e_2 = e_1
+            e_1 = target.item() - output.item()
 
-                train_output_list.append(output.cpu().detach().numpy())
-                train_target_list.append(target.cpu().detach().numpy())
+            train_output_list.append(output.cpu().detach().numpy())
+            train_target_list.append(target.cpu().detach().numpy())
 
-                loss = criterion(output, target)
-                loss += penalty
+            loss = criterion(output, target)
+            loss += penalty
 
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-            train_target_list = np.array(train_target_list).ravel()
-            train_output_list = np.array(train_output_list).ravel()
+        train_target_list = np.array(train_target_list).ravel()
+        train_output_list = np.array(train_output_list).ravel()
 
-            train_target_list = scaler.inverse_transform(train_target_list.reshape(-1, 1))
-            train_output_list = scaler.inverse_transform(train_output_list.reshape(-1, 1))
+        train_target_list = scaler.inverse_transform(train_target_list.reshape(-1, 1))
+        train_output_list = scaler.inverse_transform(train_output_list.reshape(-1, 1))
 
-            train_mse = mse(train_target_list, train_output_list)
-            train_mae = mae(train_target_list, train_output_list)
-            train_mape = mape(train_target_list, train_output_list)
-            print(f"traim mse {train_mse}")
-            print(f"train mae {train_mae}")
-            print(f"train mape {train_mape}")
-            print("--------")
+        train_mse = mse(train_target_list, train_output_list)
+        train_mae = mae(train_target_list, train_output_list)
+        train_mape = mape(train_target_list, train_output_list)
+        print(f"traim mse {train_mse}")
+        print(f"train mae {train_mae}")
+        print(f"train mape {train_mape}")
+        print("--------")
 
-            train_losses.append((train_mse, train_mae, train_mape))
+        train_losses.append((train_mse, train_mae, train_mape))
 
-            
-            tree.eval()
-            correct = 0.0
-            output_list = []
-            target_list = []
+        
+        tree.eval()
+        correct = 0.0
+        output_list = []
+        target_list = []
 
-            for batch_idx, (data, target) in enumerate(test_loader):
-                data[0][-3] = e_1
-                data[0][-2] = e_2
-                data[0][-1] = e_3
+        for batch_idx, (data, target) in enumerate(test_loader):
+            data[0][-3] = e_1
+            data[0][-2] = e_2
+            data[0][-1] = e_3
 
-                batch_size = data.size()[0]
-                data, target = data.to(device), target.to(device)
+            batch_size = data.size()[0]
+            data, target = data.to(device), target.to(device)
 
-                output = tree.forward(data)
+            output = tree.forward(data)
 
-                e_3 = e_2
-                e_2 = e_1
-                e_1 = target.item() - output.item()
+            e_3 = e_2
+            e_2 = e_1
+            e_1 = target.item() - output.item()
 
-                output_list.append(output.cpu().detach().numpy())
-                target_list.append(target.cpu().detach().numpy())
+            output_list.append(output.cpu().detach().numpy())
+            target_list.append(target.cpu().detach().numpy())
 
-            target_list = np.array(target_list).ravel()
-            output_list = np.array(output_list).ravel()
+        target_list = np.array(target_list).ravel()
+        output_list = np.array(output_list).ravel()
 
-            target_list = scaler.inverse_transform(target_list.reshape(-1, 1))
-            output_list = scaler.inverse_transform(output_list.reshape(-1, 1))
+        target_list = scaler.inverse_transform(target_list.reshape(-1, 1))
+        output_list = scaler.inverse_transform(output_list.reshape(-1, 1))
 
-            test_mse = mse(target_list, output_list)
-            test_mae = mae(target_list, output_list)
-            test_mape = mape(target_list, output_list)
-            print(f"test mse {test_mse}")
-            print(f"test mae {test_mae}")
-            print(f"test mape {test_mape}")
+        test_mse = mse(target_list, output_list)
+        test_mae = mae(target_list, output_list)
+        test_mape = mape(target_list, output_list)
+        print(f"test mse {test_mse}")
+        print(f"test mae {test_mae}")
+        print(f"test mape {test_mape}")
 
-            test_losses.append((test_mse, test_mae, test_mape))
+        test_losses.append((test_mse, test_mae, test_mape))
 
-            print("###############")
+        print("###############")
 
-        plt.plot(target_list)
-        plt.plot(output_list)
-        plt.savefig(f"{result_folder}/predictions_{M4_index}.png")
-        plt.cla()
-        f = open(f"{result_folder}/{result_txt_file}", "a+")
-        f.write("########\n")
-        f.write(f"M4 index: {M4_index}\n")
+    plt.plot(target_list)
+    plt.plot(output_list)
+    plt.savefig(f"{result_folder}/predictions_{M4_index}.png")
+    plt.cla()
+    f = open(f"{result_folder}/{result_txt_file}", "a+")
+    f.write("########\n")
+    f.write(f"M4 index: {M4_index}\n")
 
-        f.write("Train Losses")
-        f.write(str(train_losses) + "\n\n")
+    f.write("Train Losses")
+    f.write(str(train_losses) + "\n\n")
 
-        f.write("Test Losses")
-        f.write(str(test_losses) + "\n\n")
-        f.write("########\n")
-        f.close()
+    f.write("Test Losses")
+    f.write(str(test_losses) + "\n\n")
+    f.write("########\n")
+    f.close()
 
-        test_losses = [l[0] for l in test_losses]
-        train_losses = [l[0] for l in train_losses]
+    test_losses = [l[0] for l in test_losses]
+    train_losses = [l[0] for l in train_losses]
 
-        plt.plot(test_losses, label="test")
-        plt.plot(train_losses, label="train")
-        plt.legend()
-        plt.xlabel("Epochs")
-        plt.ylabel("MSE Losses")
-        plt.savefig(f"{result_folder}/SDT_result_{M4_index}.png")
-        plt.cla()
+    plt.plot(test_losses, label="test")
+    plt.plot(train_losses, label="train")
+    plt.legend()
+    plt.xlabel("Epochs")
+    plt.ylabel("MSE Losses")
+    plt.savefig(f"{result_folder}/SDT_result_{M4_index}.png")
+    plt.cla()
 
